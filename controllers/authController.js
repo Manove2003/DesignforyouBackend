61e08651cf45aa992214458716e12f6ae1b7e2e4
Manove Kumar <kumarmanove2003@gmail.com>
2024-10-04 22:25:16 +0500

    first last backend 1

--- controllers/authController.js
+++ controllers/authController.js
@@ -133,7 +133,7 @@ exports.googleCallback = async (req, res) => {
 
     // Redirect to the frontend with the token and current role
     res.redirect(
-      `http://localhost:3000/welcome?token=${token}&role=${user.role}`
+      `https://imaginative-puffpuff-805543.netlify.app/welcome?token=${token}&role=${user.role}`
     );
   } catch (error) {
     console.error("Google callback error:", error);
